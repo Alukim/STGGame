289cd4ba97e907b1b07d7a7d99c2001ea5652daf
Alukim <bartoszkowalski@outlook.com>
2016-02-26 13:09:33 +0100

    Teraz spróbuj. :P

---
+++ BitSimulator/BitSimulator/Game.cpp
@@ -0,0 +1,91 @@
+#include "Game.h"
+#include <Windows.h>
+#include <string>
+
+Game::Game()
+{
+	status = END;
+
+	if (!Font.loadFromFile("comic.ttf"))
+	{
+		MessageBox(NULL, "Font not found!", "ERROR", NULL);
+		return;
+	}
+
+	status = MENU;
+}
+
+void Game::runGame()
+{
+	while (status != END)
+	{
+		switch (status)
+		{
+		case GameState::MENU:
+			menu();
+			break;
+		case GameState::GAME:
+			//
+			break;
+		}
+	}
+}
+
+void Game::menu()
+{
+	sf::Text title("BitSimulator", Font, 80);
+	title.setStyle(sf::Text::Bold);
+
+	title.setPosition(1280 / 2 - title.getGlobalBounds().width / 2, 20);
+
+	const int ile = 2; //ile elementów masz w MENU
+
+	sf::Text tekst[ile];
+
+	std::string str[] = { "Play","Exit" };
+	for (int i = 0;i<ile;i++)
+	{
+		tekst[i].setFont(Font);
+		tekst[i].setCharacterSize(65);
+
+		tekst[i].setString(str[i]);
+		tekst[i].setPosition(1280 / 2 - tekst[i].getGlobalBounds().width / 2, 250 + i * 120);
+	}
+
+	while (status == MENU)
+	{
+		sf::Vector2f mouse(sf::Mouse::getPosition(/*mo¿emy tutaj podaæ okno*/));
+		sf::Event event;
+
+		while (Window->pollEvent(event))
+		{
+			//Wciœniêcie ESC lub przycisk X
+			if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed &&
+				event.key.code == sf::Keyboard::Escape)
+				status = END;
+
+			//klikniêcie EXIT
+			else if (tekst[1].getGlobalBounds().contains(mouse) &&
+				event.type == sf::Event::MouseButtonReleased && event.key.code == sf::Mouse::Left)
+			{
+				status = END;
+			}
+		}
+		for (int i = 0;i<ile;i++)
+			if (tekst[i].getGlobalBounds().contains(mouse))
+				tekst[i].setColor(sf::Color::Cyan);
+			else tekst[i].setColor(sf::Color::White);
+
+			Window->clear();
+
+			Window->draw(title);
+			for (int i = 0;i<ile;i++)
+				Window->draw(tekst[i]);
+
+			Window->display();
+	}
+}
+
+Game::~Game()
+{
+}
--- BitSimulator/BitSimulator/Game.h
+++ BitSimulator/BitSimulator/Game.h
@@ -5,10 +5,10 @@ class Game
 public:
 	Game();
 	~Game();
+	sf::RenderWindow *Window;
 	void runGame();
 protected:
 	enum GameState {MENU,GAME_OVER,GAME,END} status; //Lista wyliczeniowa statusu gry
-	sf::RenderWindow *window;
 private:
 	sf::Font Font;
 
