59500ea71e856b572176dfc66ecdd54d8c470310
sacktap <aleksander.krawiec12@gmail.com>
2016-02-26 16:44:39 +0100

    pierwsze gui

--- BitSimulator/BitSimulator/GUI.h
+++ BitSimulator/BitSimulator/GUI.h
@@ -2,13 +2,22 @@
 #include <SFML\Graphics.hpp>
 
 // class used for drawing meters filled with spectrum's colors
-class Meter
+class Buffer
 {
-	int fullfillment;
+	int load;
 	int capacity;
 	sf::IntRect dim;
 public:
 
-	//> sets alert
+	//> sets if there is an alert when meter is full
 	void SetAlert(bool state);
+
+	//> returns current fullfilment
+	int GetFullfillment();
+
+	//> adds /a value to the load
+	bool Load(int value);
+
+	//> draws buffer onto screen
+	void Draw();
 };
