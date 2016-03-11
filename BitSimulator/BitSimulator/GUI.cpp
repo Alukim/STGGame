05e02ff916c48b8aa6d7615415ca95c1bb7b294f
Alukim <bartoszkowalski@outlook.com>
2016-03-11 23:27:47 +0100

    Coś z tymi particlami mam

--- BitSimulator/BitSimulator/GUI.cpp
+++ BitSimulator/BitSimulator/GUI.cpp
@@ -217,3 +217,45 @@ void Points_class::Draw()
 	ref->draw(text);
 	change = false;
 }
+
+ParticleSystem::ParticleSystem(unsigned int count) :
+	m_particles(count),
+	m_vertices(sf::Points, count),
+	m_lifetime(sf::seconds(3)),
+	m_emitter(0, 0)
+{
+}
+
+void ParticleSystem::setEmitter(sf::Vector2f position)
+{
+	m_emitter = position;
+}
+
+void ParticleSystem::update(sf::Time elapsed)
+{
+	for (std::size_t i = 0; i < m_particles.size(); ++i)
+	{
+		// update the particle lifetime
+		Particle& p = m_particles[i];
+		p.lifetime -= elapsed;
+
+		// update the position of the corresponding vertex
+		m_vertices[i].position += p.velocity * elapsed.asSeconds();
+
+		// update the alpha (transparency) of the particle according to its lifetime
+		float ratio = p.lifetime.asSeconds() / m_lifetime.asSeconds();
+		m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
+	}
+}
+
+void ParticleSystem::draw(sf::RenderTarget & target, sf::RenderStates states) const
+{
+	// apply the transform
+	states.transform *= getTransform();
+
+	// our particles don't use a texture
+	states.texture = NULL;
+
+	// draw the vertex array
+	target.draw(m_vertices, states);
+}
--- BitSimulator/BitSimulator/GUI.h
+++ BitSimulator/BitSimulator/GUI.h
@@ -61,4 +61,30 @@ public:
 	void Draw();
 };
 
+class ParticleSystem : public sf::Drawable, public sf::Transformable
+{
+private:
+
+	struct Particle
+	{
+		sf::Vector2f velocity;
+		sf::Time lifetime;
+	};
+
+	std::vector<Particle> m_particles;
+	sf::VertexArray m_vertices;
+	sf::Time m_lifetime;
+	sf::Vector2f m_emitter;
+
+public:
+
+	ParticleSystem(unsigned int count);
+	void setEmitter(sf::Vector2f position);
+	void update(sf::Time elapsed);
+
+private:
+
+	virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+
+};
 
